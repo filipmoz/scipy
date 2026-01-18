06261cf0bf23c6a184d3c4d55cb864646f2d3653
filipmoz <filip@kcn.pl>
2026-01-18 11:23:03 +0100

    fix ddof in std check

--- test_stats.py
+++ test_stats.py
@@ -7,7 +7,8 @@ import numpy as np
 def test_mean_std():
     x = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
     assert np.isclose(np.mean(x), 3.0)
-    assert np.isclose(np.std(x, ddof=1), np.sqrt(2.5))
+    # sample std
+    assert np.isclose(np.std(x, ddof=1), 1.581128874, rtol=1e-5)
 
 
 def test_ttest_1samp():
