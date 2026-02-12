94f9b5d9586ef5cefb149312190337f5e30e9cdf
filipmoz <filip@kcn.pl>
2026-02-12 20:02:15 +0100

    tidy imports and add median test

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,18 @@
 """Basic scipy stats tests."""
+import numpy as np
 import pytest
 from scipy import stats
-import numpy as np
 
 
 def test_mean_std():
     x = np.array([1.0, 2.0, 3.0, 4.0, 5.0])
     assert np.isclose(np.mean(x), 3.0)
     assert np.isclose(np.std(x, ddof=1), 1.581128874, rtol=1e-5)
+
+
+def test_median():
+    x = np.array([1.0, 3.0, 2.0, 5.0, 4.0])
+    assert np.isclose(np.median(x), 3.0)
 
 
 def test_ttest_1samp():
